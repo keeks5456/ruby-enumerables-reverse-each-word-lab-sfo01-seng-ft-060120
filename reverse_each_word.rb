dbe5f56ba28b2550b406c7423820daf89441e776
Akiko Green <akikogreen08@gmail.com>
2020-05-03 21:17:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,35 +1,34 @@
 require 'pry'
 
-def reverse_each_word(sentence1)
-split_array = sentence1.split(' ')
-reversed = []
-split_array.each do |word|
-  reversed << word.reverse
-    binding.pry
-  end
-  return reversed.join(' ')
-end
+# def reverse_each_word(sentence1)
+# split_array = sentence1.split(' ')
+# reversed = []
+# split_array.each do |word|
+#   reversed << word.reverse
+#     binding.pry
+#   end
+#   return reversed.join(' ')
+# end
 
 # refractured
 
-# def reverse_each_word(sentence1)
-#     reversed = []
-#   split_array = sentence1.split(' ').each{|word| reversed << word.reverse}
-#   reversed.join(' ')
-# end
-
-
-
 def reverse_each_word(sentence1)
-  split_array = sentence1.split(' ')
-  split_array.collect do |word|
-    word.reverse!
-  end
-  return split_array.join(' ')
+    reversed = []
+  split_array = sentence1.split(' ').each{|word| reversed << word.reverse}.join(' ')
 end
+
+
+
+# def reverse_each_word(sentence1)
+#   split_array = sentence1.split(' ')
+#   split_array.collect do |word|
+#     word.reverse!
+#   end
+#   return split_array.join(' ')
+# end
 
 # refractured
 
-# def reverse_each_word(sentence1)
-# split_array = sentence1.split(' ').collect{|word| word.reverse!} split_array.join(' ')
-# end
+def reverse_each_word(sentence1)
+split_array = sentence1.split(' ').collect{|word| word.reverse!}.join(' ')
+end
