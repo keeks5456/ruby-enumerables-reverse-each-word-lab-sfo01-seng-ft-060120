237d57f07df0df94b6e4c211b1d206c9ca95574c
Akiko Green <akikogreen08@gmail.com>
2020-05-03 21:15:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,22 +1,22 @@
 require 'pry'
 
-# def reverse_each_word(sentence1)
-# split_array = sentence1.split(' ')
-# reversed = []
-# split_array.each do |word|
-#   reversed << word.reverse
-#     binding.pry
-#   end
-#   return reversed.join(' ')
-# end
+def reverse_each_word(sentence1)
+split_array = sentence1.split(' ')
+reversed = []
+split_array.each do |word|
+  reversed << word.reverse
+    binding.pry
+  end
+  return reversed.join(' ')
+end
 
 # refractured
 
-def reverse_each_word(sentence1)
-    reversed = []
-  split_array = sentence1.split(' ').each{|word| reversed << word.reverse}
-  reversed.join(' ')
-end
+# def reverse_each_word(sentence1)
+#     reversed = []
+#   split_array = sentence1.split(' ').each{|word| reversed << word.reverse}
+#   reversed.join(' ')
+# end
 
 
 
