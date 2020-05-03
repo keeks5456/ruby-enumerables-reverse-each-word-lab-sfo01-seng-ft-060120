7d4ec38a5cca90c9ce3e7fe99dc8ab911d14a057
Akiko Green <akikogreen08@gmail.com>
2020-05-03 21:17:43 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,36 @@
+require 'pry'
+
+# def reverse_each_word(sentence1)
+# split_array = sentence1.split(' ')
+# reversed = []
+# split_array.each do |word|
+#   reversed << word.reverse
+#     binding.pry
+#   end
+#   return reversed.join(' ')
+# end
+
+# refractured
+
+def reverse_each_word(sentence1)
+    reversed = []
+  split_array = sentence1.split(' ').each{|word| reversed << word.reverse}.join(' ')
+end
+
+
+
+# def reverse_each_word(sentence1)
+#   split_array = sentence1.split(' ')
+#   split_array.collect do |word|
+#     word.reverse!
+#   end
+#   return split_array.join(' ')
+# end
+
+
+
+# refractured
+
+def reverse_each_word(sentence1)
+split_array = sentence1.split(' ').collect{|word| word.reverse!}.join(' ')
+end
